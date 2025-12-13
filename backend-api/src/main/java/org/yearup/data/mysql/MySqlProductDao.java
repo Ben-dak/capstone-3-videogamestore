055a11058d6f4664d12188749ca0efc44ce78385
Ben-dak <dominickfrench.oe@gmail.com>
2025-12-13 15:16:52 -0600

    Fixed search method bugs that returned incorrect products and no maxprice in the MySqlProductDao class.

--- backend-api/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ backend-api/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -23,40 +23,46 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     {
         List<Product> products = new ArrayList<>();
 
+        // no sign for maximum was added
         String sql = "SELECT * FROM products " +
-                "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
-                "   AND (subcategory = ? OR ? = '') ";
+                "WHERE (category_id = ? OR ? = -1)" +
+                " AND (price >= ? OR ? = -1) " +
+                " AND (price <= ? OR ? = -1) " +
+                " AND (subcategory = ? OR ? = '') ";
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
         maxPrice = maxPrice == null ? new BigDecimal("-1") : maxPrice;
         subCategory = subCategory == null ? "" : subCategory;
 
-        try (Connection connection = getConnection())
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql))
         {
-            PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, categoryId);
             statement.setInt(2, categoryId);
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, subCategory);
-            statement.setString(6, subCategory);
 
-            ResultSet row = statement.executeQuery();
+            // this is why there was no maxprice in query stateme
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+            statement.setString(7, subCategory);
+            statement.setString(8, subCategory);
 
-            while (row.next())
-            {
-                Product product = mapRow(row);
-                products.add(product);
+            // try for the while
+            try(ResultSet row = statement.executeQuery()) {
+
+                while (row.next()) {
+                    Product product = mapRow(row);
+                    products.add(product);
+                }
+                return products;
             }
         }
         catch (SQLException e)
         {
             throw new RuntimeException(e);
         }
-
-        return products;
     }
 
     @Override
